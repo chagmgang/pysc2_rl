ad40fffd9451a99acb17534abd04da76b6e2cd2a
ckg <chagmgang@gmail.com>
2018-06-23 19:09:18 +0900

    add ubuntu version

---
+++ Move2Beacon_A2C_onpolicy_ubuntu/Move2Beacon.py
@@ -0,0 +1,95 @@
+import sys
+from absl import flags
+from pysc2.env import sc2_env, environment
+from pysc2.lib import actions, features
+import matplotlib.pyplot as plt
+from action_group import actAgent2Pysc2, no_operation
+from state_group import obs2state, obs2distance
+import numpy as np
+import random
+import tensorflow as tf
+from collections import deque
+import time
+import math
+from A2C import Actor, Critic
+# Define the constant
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
+_UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
+_SELECTED_UNIT = features.SCREEN_FEATURES.selected.index
+friendly = 1
+neutral = 3
+_SELECT_RECT = actions.FUNCTIONS.select_rect.id
+_SELECT_POINT = actions.FUNCTIONS.select_point.id
+_SELECT_ARMY = actions.FUNCTIONS.select_army.id
+_MOVE_SCREEN = actions.FUNCTIONS.Move_screen.id
+_NO_OP           = actions.FUNCTIONS.no_op.id
+_ATTACK_MINIMAP = actions.FUNCTIONS.Attack_minimap.id
+_SELECT_ALL  = [0]
+_NOT_QUEUED  = [0]
+step_mul = 4
+FLAGS = flags.FLAGS
+EPISODES = 10000
+BATCH_SIZE = 500
+
+# main function, create env, define model, learn from observation and save model
+def train():
+    FLAGS(sys.argv)
+    with sc2_env.SC2Env(
+                        map_name='MoveToBeacon',
+                        agent_interface_format=sc2_env.parse_agent_interface_format(
+                            feature_screen=84,
+                            feature_minimap=64,
+                            rgb_screen=None,
+                            rgb_minimap=None,
+                            action_space=None,
+                            use_feature_units=False),
+                        step_mul=step_mul,
+                        game_steps_per_episode=None,
+                        disable_fog=False,
+                    visualize=True) as env:
+        sess = tf.Session()
+        actor = Actor(sess, n_features=2, n_actions=4, lr=0.001)
+        critic = Critic(sess, n_features=2, lr=0.001)
+        sess.run(tf.global_variables_initializer())
+        for episodes in range(EPISODES):
+            done = False
+            obs = env.reset()
+            while not 331 in obs[0].observation["available_actions"]:
+                actions = actAgent2Pysc2(100,obs)
+                obs = env.step(actions=[actions])
+            state = np.array(obs2state(obs))
+            print('episode start')
+            global_step = 0
+            reward = 0
+            while not done: 
+                global_step += 1
+                time.sleep(0.2)
+                action = actor.choose_action(state)
+                actions = actAgent2Pysc2(action,obs)
+                obs = env.step(actions=[actions])
+                for i in range(3):
+                    actions = no_operation(obs)
+                    obs = env.step(actions=[actions])
+                distance = obs2distance(obs)
+                if global_step == 1:
+                    pre_distance = distance
+                next_state = np.array(obs2state(obs))
+                reward = -(distance-pre_distance)*400
+                
+                if distance < 0.03 or global_step == 200:   # 게임 종료시
+                    if distance < 0.03:
+                        reward = 10
+                    if global_step == 200:
+                        reward = -10
+                    done = True
+                
+                td_error = critic.learn(state, reward, next_state)
+                actor.learn(state, action, td_error)
+
+                if distance < 0.03 or global_step == 200:   # 게임 종료시
+                    break
+                state = next_state
+                pre_distance = distance
+
+if __name__ == '__main__':
+    train()
---
+++ Move2Beacon_A2C_onpolicy_ubuntu/action_group.py
@@ -0,0 +1,59 @@
+import sys
+from absl import flags
+from pysc2.env import sc2_env
+from pysc2.lib import actions, features
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Define the constant
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
+_UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
+friendly = 1
+neutral = 3
+_SELECTED_UNIT = features.SCREEN_FEATURES.selected.index
+
+_SELECT_POINT = actions.FUNCTIONS.select_point.id
+_SELECT_ARMY = actions.FUNCTIONS.select_army.id
+_MOVE_SCREEN = actions.FUNCTIONS.Move_screen.id
+_NO_OP           = actions.FUNCTIONS.no_op.id
+_RALLY_UNITS_SCREEN = actions.FUNCTIONS.Rally_Units_screen.id
+
+
+_SELECT_ALL  = [0]
+_NOT_QUEUED  = [0]
+
+def no_operation(obs):
+    action = actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
+    return action
+
+def move_unit(obs, mode):       # mode= 1,2,3,4 & up,down,left,right
+
+    selected_unit_position_y, selected_unit_position_x = (
+                obs[0].observation["screen"][_SELECTED_UNIT] == True).nonzero()
+    target_x, target_y = np.mean(selected_unit_position_x), np.mean(selected_unit_position_y)
+
+    if mode == 1:   #up
+        dest_x, dest_y = np.clip(target_x, 0, 63), np.clip(target_y - 10, 0, 63)
+    elif mode == 2: #down
+        dest_x, dest_y = np.clip(target_x, 0, 63), np.clip(target_y + 10, 0, 63)
+    elif mode == 3: #left
+        dest_x, dest_y = np.clip(target_x - 10, 0, 63), np.clip(target_y, 0, 63)
+    elif mode == 4: #right
+        dest_x, dest_y = np.clip(target_x + 10, 0, 63), np.clip(target_y, 0, 63)
+    action = actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, [dest_x, dest_y]])  # move Up
+
+    return action
+
+def actAgent2Pysc2(i, obs):
+    if i == 0:
+        action = move_unit(obs, 1)
+    elif i == 1:
+        action = move_unit(obs, 2)
+    elif i == 2:
+        action = move_unit(obs, 3)
+    elif i == 3:
+        action = move_unit(obs, 4)
+    elif i ==100:
+        action = actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
+    return action
